33255664ce9abed6e48654bf74563f4521d1f8ef
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:08:39 +0930

    Update question08.sh

--- question08.sh
+++ question08.sh
@@ -1,10 +1,4 @@
 mkdir dir2
 mv *.txt dir2/
-git commit -m "Moved all text files to dir2"
-echo '#!/bin/bash' > move_and_commit_text_files.sh
-echo 'mkdir dir2' >> move_and_commit_text_files.sh
-echo 'mv *.txt dir2/' >> move_and_commit_text_files.sh
-echo 'git add dir2/*.txt' >> move_and_commit_text_files.sh
-echo 'git commit -m "Moved all text files to dir2"' >> move_and_commit_text_files.sh
-chmod +x move_and_commit_text_files.sh
-./move_and_commit_text_files.sh
+git add dir2/*.txt
+git commit -m "Moved all .txt files to dir2"
