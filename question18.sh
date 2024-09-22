cb2d223d09ae8f4527326f6b6fba02b17a66830a
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:17:54 +0930

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -1,15 +1,15 @@
 git fetch --all
 git branch -r
 git checkout main
-for branch in $(git branch -r | grep "ready"); do
-  git merge --no-ff ${branch#origin/}
+for branch in $(git branch -r | grep 'origin/ready'); do
+    git merge ${branch#origin/} --no-ff -m "Merging ${branch#origin/} into main"
 done
-for branch in $(git branch -r | grep "ready"); do
-  git push origin --delete ${branch#origin/}
-  git branch -d ${branch#origin/}
+for branch in $(git branch -r | grep 'origin/ready'); do
+    git push origin --delete ${branch#origin/}
+    git branch -D ${branch#origin/}  # Force delete if necessary
 done
-for branch in $(git branch -r | grep "update"); do
-  git checkout ${branch#origin/}
-  git merge main
+for branch in $(git branch -r | grep 'origin/update'); do
+    git checkout ${branch#origin/}
+    git merge main -m "Updating ${branch#origin/} from main"
 done
 git checkout main
