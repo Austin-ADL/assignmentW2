75ddf31658e00a3fe064256bece4986ffebaa806
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:09:22 +0930

    Update question12.sh

--- question12.sh
+++ question12.sh
@@ -1,11 +1,4 @@
 git checkout branch2
 git stash pop
-git add file4
-git commit -m "Restored and committed modifications to file4"
-echo '#!/bin/bash' > restore_and_commit_changes.sh
-echo 'git checkout branch2' >> restore_and_commit_changes.sh
-echo 'git stash pop' >> restore_and_commit_changes.sh
-echo 'git add file4' >> restore_and_commit_changes.sh
-echo 'git commit -m "Restored and committed modifications to file4"' >> restore_and_commit_changes.sh
-chmod +x restore_and_commit_changes.sh
-./restore_and_commit_changes.sh
+git add .
+git commit -m "Restored and committed previously uncommitted changes"
