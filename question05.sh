672f5d1831d99949c7c1aa581082ef16bc8526ae
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:35:43 +0930

    Update question05.sh

--- question05.sh
+++ question05.sh
@@ -1,9 +1,3 @@
 mkdir dir1
 touch dir1/file2
 git add dir1
-echo '#!/bin/bash' > create_and_stage_dir.sh
-echo 'mkdir dir1' >> create_and_stage_dir.sh
-echo 'touch dir1/file2' >> create_and_stage_dir.sh
-echo 'git add dir1' >> create_and_stage_dir.sh
-chmod +x create_and_stage_dir.sh
-./create_and_stage_dir.sh
