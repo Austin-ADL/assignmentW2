4a9ae304b42e1e2e79e06c55671e9aa9d37873e3
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:18:39 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,15 +1,15 @@
 git checkout -b branch1
-rm -rf dir1/dir2
-mv dir1/dir2/foo dir1/
-rm dir3/bar_copy
-touch newfile1
+mv dir1/dir2/foo dir1/foo  # Move foo up one directory level
+rm dir3/bar_copy          # Remove bar_copy from dir3
+touch newfile1            # Create newfile1
 git add .
-git commit -m "Configured branch1 structure and added newfile1"
+git commit -m "Configured files for branch1"
 git checkout main
 git checkout -b branch2
-mv dir1/dir2/foo dir1/dir2/foo_modified
-mkdir -p dir1/dir3
-touch dir1/dir3/newfile2
+echo "Modified content" > dir1/dir2/foo_modified  # Create modified version of foo
+mv dir1/dir2/foo dir1/dir2/foo_modified          # Rename foo to foo_modified
+mkdir dir1/dir3                                  # Create new directory under dir1
+touch dir1/dir3/newfile2                         # Create newfile2 in new dir3
 git add .
-git commit -m "Configured branch2 structure with foo_modified and newfile2"
+git commit -m "Configured files for branch2"
 git checkout main
