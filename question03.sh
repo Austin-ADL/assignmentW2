de1c9a99fd37388ab1abb686c7dc8fc1274faa81
Austin-ADL <xiyuan.zhang01@student.adelaide.edu.au>
2024-09-22 19:12:53 +0930

    Update question03.sh

--- question03.sh
+++ question03.sh
@@ -1,7 +1,2 @@
 touch file3.txt
 git add file3.txt
-echo '#!/bin/bash' > create_and_stage_file.sh
-echo 'touch file3.txt' >> create_and_stage_file.sh
-echo 'git add file3.txt' >> create_and_stage_file.sh
-chmod +x create_and_stage_file.sh
-./create_and_stage_file.sh
